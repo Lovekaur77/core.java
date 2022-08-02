7006a51c69072e9977261c5c3a80e64d749e6346
Lovekaur77 <109948338+Lovekaur77@users.noreply.github.com>
2022-08-02 14:03:54 +0530

    2nd assignment

---
+++ Renuka.java
@@ -0,0 +1,116 @@
+//que 1
+import java.util.Scanner;
+class Main
+{
+   public static void main(String [] args)
+    {
+	Scanner sc= new Scanner(System.in);
+	    System.out.println(" Enter a number");
+		
+		int num = sc.nextInt();
+		if(num% 2 ==0)
+		
+		   System.out.println(num + " is even");
+		   else
+		   System.out.println(num + " is odd");
+		   
+		   }
+		  }
+		   
+		
+//que 5
+pass by value - All object refrences in passed by value. This means that copy of the value will passed the
+method.
+pass by reference- location is a variable is passed to a function  as arguments,it is called pass by reference.
+
+//que 2
+class Test
+{
+   public static void main(String args[]) 
+   {
+	   Scanner sc = new Scanner(System.in);
+	     System.out.println("Enter the number");
+		   int number = sc.nextInt();
+		 
+		 if(number>55){
+			 System.out.println("Number is greater than 55");
+		 }
+		 else {
+			 System.out.println("Number is less than 55");
+		 }
+   }
+ //que 7
+ public class PrintArray{
+	 public static void main(String[]args) {
+		 
+		 int[]arr = new int[]{1,2,3,4,5,6,7,8,9,10};
+		 
+		    System.out.println(" Elements of given array");
+			for(i=0;i<arr.length;i++)
+			{
+				System.out.println(arr[i] + "");
+			}
+	 }
+			
+			
+			
+			
+			
+//que4
+class SwitchPoint
+{
+  public static void main(String args[])
+  {
+   Scanner sc = new scanner(System.in);
+   
+     System.out.println("Enter a number");
+	 
+      int rollNo = sc.nextInt();
+      switch (rollno) {
+    case:579 : (System.outprintln("Sonu's Rollno");
+	break;
+	case:546 :(System.out.println("Monu's Rollno");
+	break;
+	case:553 :(System.out.println("Manu's Rollno");
+	break;
+	case:544 :(System.out.println("Meenu's Rollno");
+    break;
+	case:516 :(System.out.println("Meena's Rollno");
+	break;
+	default;
+	break;
+	  }
+  }
+  //que6
+  ClassDoWhile{
+	  public static void main(String[]args){
+		  inti=1;
+		  do{
+			   System.out.println(i);
+			   i++;
+		  }while(i<=8);
+	  }
+  }
+		  
+			 
+			 
+	   
+	   
+		
+		
+		
+		
+//que 8 class Numeric12
+//{
+     // public static void main(String args[])
+  // {
+    // for(int i=1;i<=3;i++)
+   // {
+    // for(int j=1;j<=5;j++)
+		//if(j%2==0)
+	// {
+	   // System.out.print("1");
+	}
+	   //System.out.println()
+   }
+  }
